2ac2c0dc03ad47abe90b9877f3b5547d05496c0a
Tyler Gandy <tylerbgandy@gmail.com>
2022-06-29 11:30:50 -0400

    initial animation for star logo. will replace with B soon

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -10,20 +10,39 @@ left:1rem;
 
 width:100%
 color: ${(props) => props.theme.text}
-z-index:5;`;
+z-index:5;
+
+svg{
+    width:4rem;
+    height:auto;
+    overflow: visible;
+    stroke-linejoin: round;
+    stroke-linecap: round;
+    g{
+        path{
+            stroke: ${(props) => props.theme.text};
+        }
+    }
+    
+}`;
 
 const Logo = () => {
   return (
     <Container>
       <Link to="/">
-        {/* <img
-          src={LogoBB}
-          alt="logo"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
           enable-background="new 0 0 24 24"
-          height="60px"
+          height="48px"
           viewBox="0 0 24 24"
           width="48px"
-        ></img> */}
+          fill="none"
+        >
+          <g></g>
+          <g>
+            <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z" />
+          </g>
+        </svg>
       </Link>
     </Container>
   );
