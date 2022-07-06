207c620a423cd47554ec0e7a6b1a57d0134a1872
Tyler Gandy <tylerbgandy@gmail.com>
2022-07-06 19:19:06 -0400

    changes to about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -8,24 +8,36 @@ import "../index.css";
 const Section = styled.section`
   position: relative;
   min-height: 100vh;
-  width: 80vw;
+  width: 100vw;
   overflow: hidden;
+  background-color: black;
 
   display: flex;
+  margin: 0 auto;
 `;
 
 const Title = styled.h1`
-  font-size: 5rem;
+  font-size: 5.5rem;
   font-family: "Splash";
   font-weight: 300;
+  color: white;
 
   position: absolute;
   top: 1rem;
   left: 5%;
   z-index: 5;
+  margin-left: 11%;
 `;
 const Left = styled.div`
   width: 50%;
+  font-size: 1.5rem;
+  font-family: "righteous";
+  color: white;
+  font-weight: 280;
+  position: relative;
+  z-index: 5;
+  margin-top: 20%;
+  margin-left: 10%;
 `;
 const Right = styled.div`
   width: 50%;
@@ -33,7 +45,7 @@ const Right = styled.div`
 
 function About() {
   return (
-    <Section id="about">
+    <Section id="about#fixed-target">
       {/* <section id="about"> */}
       <Title
         data-scroll
@@ -42,7 +54,13 @@ function About() {
       >
         About Us
       </Title>
-      <Left>Text</Left>
+      <Left data-scroll data-scroll-sticky data-scroll-target="#fixed-target">
+        We are the innovation for a broken system, cooperatively and
+        persistently working to reinvent the music industry as we know it.
+        <br />
+        <br /> We are more than dedicated to helping build a better future for
+        us all to thrive and prosper. #freeeverything
+      </Left>
       <Right>Images</Right>
       <Nav />
       {/* </section> */}
