d055c0771cb4e2ce696e89d04e82e0ba2b8751aa
Tyler Gandy <tylerbgandy@gmail.com>
2022-07-21 14:25:04 -0400

    changes made to Music compnent

--- src/sections/Music.jsx
+++ src/sections/Music.jsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 import Nav from "../components/nav/Nav";
 import "../components/nav/Nav.css";
 import "./Music.css";
+import { SiApplemusic, SiSpotify } from "react-icons/si";
+import { RiSoundcloudFill } from "react-icons/ri";
+import Logo1 from "../assets/Images/blackAppleMd.png";
+import Logo2 from "../assets/Images/spotifyBlackMd.jpg";
+import Logo3 from "../assets/Images/Soundcloud14.jpg";
+
 // import Avatar1 from "../../assets/2.png";
 // import Avatar2 from "../../assets/6.png";
 // import Avatar3 from "../../assets/4.png";
@@ -24,16 +30,19 @@ const Section = styled.section`
 
 const data = [
   {
+    logo: Logo1,
     name: "Apple Music",
-    review: "Apple Music.",
+    review: "Click for Apple",
   },
   {
+    logo: Logo2,
     name: "Spotify",
-    review: "Spotify",
+    review: "Click for Spotify",
   },
   {
+    logo: Logo3,
     name: "SoundCloud",
-    review: "SoundCloud",
+    review: "Click for SoundCloud",
   },
 ];
 
@@ -54,18 +63,62 @@ const Music = () => {
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={() => console.log("slide change")}
         >
-          {data.map(({ avatar, name, review, index }) => {
-            return (
-              <SwiperSlide key={index} className="testimonyArticle">
-                <div className="client__avatar">
-                  <img src="" alt={name}></img>
-                </div>
+          {/* {data.map(({ logo, name, review, index }) => { */}
+          <SwiperSlide key="index" className="testimonyArticle">
+            <div className="music__logo">
+              <img
+                src={Logo1}
+                alt="Apple Music"
+                onClick={() =>
+                  window.open(
+                    "https://music.apple.com/us/artist/basement-bangers/1622378502"
+                  )
+                }
+                target="blank"
+              ></img>
+            </div>
 
-                <h5 className="client__name">{name}</h5>
-                <small className="client__review">{review}</small>
-              </SwiperSlide>
-            );
-          })}
+            <h5 className="client__name">Apple Music</h5>
+            <small className="client__review">click for Apple</small>
+            {/* <button>click here</button> */}
+          </SwiperSlide>
+          <SwiperSlide key="index" className="testimonyArticle">
+            <div className="music__logo">
+              <img
+                src={Logo2}
+                alt="Spotify"
+                onClick={() =>
+                  window.open(
+                    "https://open.spotify.com/artist/0SN1va4GmDZQr0fELEdfZ2?si=sAPZLqPiRDOg-ygnKkp9Dg&nd=1"
+                  )
+                }
+                target="blank"
+              ></img>
+            </div>
+
+            <h5 className="client__name">Spotify</h5>
+            <small className="client__review">click for Spotify</small>
+            {/* <button>click here</button> */}
+          </SwiperSlide>
+          <SwiperSlide key="index" className="testimonyArticle">
+            <div className="music__logo">
+              <img
+                src={Logo3}
+                alt="SoundCloud"
+                onClick={() =>
+                  window.open(
+                    "https://soundcloud.com/basement-bangers-497185316"
+                  )
+                }
+                target="blank"
+              ></img>
+            </div>
+
+            <h5 className="client__name">Sound Cloud</h5>
+            <small className="client__review">click for SoundCloud</small>
+            {/* <button>click here</button> */}
+          </SwiperSlide>
+          );
         </Swiper>
       </div>
       <div>
