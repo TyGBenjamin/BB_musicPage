6fe19dd0dd2f68e671fe108cf65de86021126b03
Tyler Gandy <tylerbgandy@gmail.com>
2022-08-31 02:47:57 -0400

    added swipe to Shop will update tomorrow and get joib finished

--- src/sections/Shop.jsx
+++ src/sections/Shop.jsx
@@ -17,6 +17,30 @@ import img1 from "../assets/Images/10.webp";
 import "swiper/css";
 import "swiper/css/pagination";
 
+import Logo1 from "../assets/Images/blackAppleMd.png";
+
+// import Avatar1 from "../../assets/2.png";
+// import Avatar2 from "../../assets/6.png";
+// import Avatar3 from "../../assets/4.png";
+
+const data = [
+  {
+    logo: Logo1,
+    name: "Apple Music",
+    review: "Click for Apple",
+  },
+  {
+    logo: Logo2,
+    name: "Spotify",
+    review: "Click for Spotify",
+  },
+  {
+    logo: Logo3,
+    name: "SoundCloud",
+    review: "Click for SoundCloud",
+  },
+];
+
 const Section = styled.section`
   min-height: 100vh;
   height: auto;
@@ -186,6 +210,74 @@ const Shop = () => {
         {/* <img src={Shop1} alt="hey" class="item">
           {" "}
         </img> */}
+        <Swiper
+          className="container testimony__container"
+          // install Swiper modules
+          modules={[Pagination]}
+          spaceBetween={5}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {/* {data.map(({ logo, name, review, index }) => { */}
+          <SwiperSlide key="index" className="testimonyArticle">
+            <div className="music__logo">
+              <img
+                src={Logo1}
+                alt="Apple Music"
+                onClick={() =>
+                  window.open(
+                    "https://music.apple.com/us/artist/basement-bangers/1622378502"
+                  )
+                }
+                target="blank"
+              ></img>
+            </div>
+
+            <h5 className="client__name">Apple Music</h5>
+            <small className="client__review">click for Apple</small>
+            {/* <button>click here</button> */}
+          </SwiperSlide>
+          <SwiperSlide key="index" className="testimonyArticle">
+            <div className="music__logo">
+              <img
+                src={Logo2}
+                alt="Spotify"
+                onClick={() =>
+                  window.open(
+                    "https://open.spotify.com/artist/0SN1va4GmDZQr0fELEdfZ2?si=sAPZLqPiRDOg-ygnKkp9Dg&nd=1"
+                  )
+                }
+                target="blank"
+              ></img>
+            </div>
+
+            <h5 className="client__name">Spotify</h5>
+            <small className="client__review">click for Spotify</small>
+            {/* <button>click here</button> */}
+          </SwiperSlide>
+          <SwiperSlide key="index" className="testimonyArticle">
+            <div className="music__logo">
+              <img
+                src={Logo3}
+                alt="SoundCloud"
+                onClick={() =>
+                  window.open(
+                    "https://soundcloud.com/basement-bangers-497185316"
+                  )
+                }
+                target="blank"
+              ></img>
+            </div>
+
+            <h5 className="client__name">Sound Cloud</h5>
+            <small className="client__review">click for SoundCloud</small>
+            {/* <button>click here</button> */}
+          </SwiperSlide>
+          );
+        </Swiper>
         <Product img={img1} title="xyz" />
         <Product img={img1} title="xyz" />
         <Product img={img1} title="xyz" />
